4fb0a2709a721906b8c68b18552234f5411f41dd
Eric Dong <eric@donger.ca>
2025-02-06 23:58:04 -0700

    comments, bgm init

--- src/initGame.js
+++ src/initGame.js
@@ -1,12 +1,18 @@
+import initMuteButton from "./components/MuteButton";
 import makeKaplayCtx from "./kaplayCtx";
 import initBedroom from "./scenes/Bedroom";
 import initHallway from "./scenes/Hallway";
 
 export default async function initGame() {
+
+    // create kaplay context and define layers
     const k = makeKaplayCtx();
     k.setLayers(["bg", "game", "fg"], "game")
     
+    // fonts
     k.loadFont('gaegu', './fonts/Gaegu-Regular.ttf')
+
+    // sprites
     k.loadSprite('player', './sprites/player_sheet.png', {
         sliceX: 2,
         sliceY: 5,
@@ -27,7 +33,14 @@ export default async function initGame() {
     k.loadSprite('dresser', './furniture/dresser.png');
     k.loadSprite('laundry', './sprites/laundry.png')
     k.loadSprite('stairs', './images/stairs.png')
+    k.loadSprite('sound_unmuted', './images/sound_unmuted.png')
+    k.loadSprite('sound_muted', './images/sound_muted.png')
 
+    // sounds
+    k.loadSound('bedroom_bgm', './sounds/bedroom_bgm.mp3')
+
+
+    // camera zoom initialization
     const setInitCamZoom = () => {
         if (k.width() < 1000) {
             k.setCamScale(0.7, 0.7);
@@ -39,9 +52,15 @@ export default async function initGame() {
 
     setInitCamZoom();
 
-
+    // scene initialization
     initBedroom(k)
     initHallway(k)
+
+    // starting game!!
     // k.go('bedroom', k.center())
     k.go('hallway', k.center().add(k.vec2(50, 0)))
+    k.play('bedroom_bgm', {
+            volume: 0.5,
+            loop: true,
+        })
 }
