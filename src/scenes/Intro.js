f85d3d7c6ba386fb249ab2f22a340b888c88c8a8
Eric Dong <eric@donger.ca>
2025-02-07 18:30:27 -0700

    implemented intro sequence

---
+++ src/scenes/Intro.js
@@ -0,0 +1,66 @@
+import createDialog from "../components/Dialogue";
+import makePlayer from "../entities/Player";
+import { isMuted } from "../ReactUI";
+
+/**
+ * 
+ * @param {import("kaplay").KAPLAYCtx} k 
+ */
+export default function initIntro(k) {
+    k.scene('intro', (bgm) => {
+        k.onUpdate(() => {
+            if (!isMuted) {
+                bgm.paused = false;
+            } else {
+                bgm.stop()
+            }
+        })
+
+        k.setCamPos(k.center())
+        makePlayer(k, k.center(), 400, 'down');
+        let dialog = null
+        let progress = -1
+        const dialog_pos = k.center().add(k.vec2(0, k.height()/2 - 100))
+        if (progress == -1) {
+            k.wait(1, () => {
+                dialog = createDialog(k, "hi baby! how are you doing? (press space)", dialog_pos, 0)
+                progress = 1
+            })
+        }
+        k.onKeyPress('space', () => {
+            k.play('interact', { volume: 1 })
+            if (progress == -1) {
+                return
+            } 
+            if (dialog != null && !dialog[0].isDestroyed) {
+                dialog[0].destroy()
+                dialog[1].destroy()
+            }
+            if (progress == 1) {
+                dialog = createDialog(k, "i really hope you enjoy this little game i made for you", dialog_pos, 0)
+            }
+            if (progress == 2) {
+                dialog = createDialog(k, "i worked pretty hard on it despite what it looks like", dialog_pos, 0)
+            }
+            if (progress == 3) {
+                dialog = createDialog(k, "i hope you have fun exploring the house", dialog_pos, 0)
+            }
+            if (progress == 4) {
+                dialog = createDialog(k, "i love you so much, have fun!!", dialog_pos, 0)
+            }
+            if (progress == 5) {
+                dialog = createDialog(k, "just remember, WASD or arrow keys to move and SPACE to interact", dialog_pos, 0)
+            }
+            if (progress == 6) {
+                bgm.stop()
+                bgm = k.play('bedroom_bgm', {
+                    volume: 0.5,
+                    loop: true,
+                    paused: true,
+                })
+                k.go('bedroom', k.center(), 'down', bgm)
+            }
+            progress++
+        })
+    })
+}
