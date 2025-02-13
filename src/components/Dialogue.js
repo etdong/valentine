d37ee7032677b5f55deaf64872d779fe6de5af16
Eric Dong <eric@donger.ca>
2025-02-13 00:11:43 -0700

    dialogue foreground change

--- src/components/Dialogue.js
+++ src/components/Dialogue.js
@@ -9,6 +9,7 @@ export default function makeDialog(k, text, lifetime = 3, size = 48) {
             k.color(0, 0, 0),
             k.fixed(),
             k.layer('fg'),
+            k.z(2)
         ])
     const dialogBox = k.add([
         k.rect(dialogText.width + 64, size + 32),
@@ -16,7 +17,8 @@ export default function makeDialog(k, text, lifetime = 3, size = 48) {
         k.pos(k.center().add(k.vec2(0, k.height()/2 - 100))),
         k.outline(4),
         k.fixed(),
-        k.layer('game'),
+        k.layer('fg'),
+        k.z(1),
         {
             isDestroyed: false
         }
